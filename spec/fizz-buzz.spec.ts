8d33120bc4d76eb358fcf9b39746e395de719e30
vernaud <40890244+vernaud@users.noreply.github.com>
2024-06-27 12:07:51 +0200

    rafactor: print function for FizzBuzz

--- spec/fizz-buzz.spec.ts
+++ spec/fizz-buzz.spec.ts
@@ -1,15 +1,15 @@
 import {FizzBuzz} from "../src/fizz-buzz";
 
 
-describe('print()', () => {
+describe('printFizzBuzzResult()', () => {
     
-    it('should log the number in param.', () => {
+    it('should log "1" when param is 1.', () => {
         // Arrange
         spyOn(console, 'log');
         const expected: string = "1";
 
         // Act        
-        FizzBuzz.print(1);
+        FizzBuzz.printFizzBuzzResult(1);
         
         // Assert
         expect(console.log).toHaveBeenCalledWith(expected);
--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,6 +1,6 @@
 export class FizzBuzz {
-    
-    static print(arg0: number): void {
+
+    static printFizzBuzzResult(arg0: number): void {
         const result = arg0.toString();
         console.log(result);
     }
