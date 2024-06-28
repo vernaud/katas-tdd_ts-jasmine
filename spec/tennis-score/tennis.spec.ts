94aa82b195c41329a249c99685f596d3548be454
vernaud <40890244+vernaud@users.noreply.github.com>
2024-06-28 13:53:17 +0200

    test: add cases [15, 30, 40] to convertPointsToScore

--- spec/tennis-score/tennis.spec.ts
+++ spec/tennis-score/tennis.spec.ts
@@ -17,11 +17,32 @@ describe('displayScoreToLog', () => {
 
 describe('convertPointsToScore',() => {
    
-    it('should return "0" when points is 0', () => {
+    it('should return 0 when points is 0', () => {
         // Act
         const score = ScoreDisplay.convertPointsToScore(0);
         // Assert
         expect(score).toBe(0);
+    });
+
+    it('should return 15 when points is 1', () => {
+        // Act
+        const score = ScoreDisplay.convertPointsToScore(1);
+        // Assert
+        expect(score).toBe(15);
+    });
+
+    it('should return 30 when points is 2', () => {
+        // Act
+        const score = ScoreDisplay.convertPointsToScore(2);
+        // Assert
+        expect(score).toBe(30);
+    });
+
+    it('should return 40 when points is 3', () => {
+        // Act
+        const score = ScoreDisplay.convertPointsToScore(3);
+        // Assert
+        expect(score).toBe(40);
     });
 
 });
