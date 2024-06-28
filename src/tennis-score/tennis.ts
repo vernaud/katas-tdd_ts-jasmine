4b0f64ab02967c45d612002e722578a5d2760da1
vernaud <40890244+vernaud@users.noreply.github.com>
2024-06-28 13:56:47 +0200

    feat: convertPointsToScore for [0,1,2,3] points

--- src/tennis-score/tennis.ts
+++ src/tennis-score/tennis.ts
@@ -1,5 +1,17 @@
 export class ScoreDisplay {
     static convertPointsToScore(arg0: number): number {
+        if (arg0 === 0) {
+            return 0;
+        }
+        if (arg0 === 1) {
+            return 15;
+        }
+        if (arg0 === 2) {
+            return 30;
+        }
+        if (arg0 === 3) {
+            return 40;
+        }
         return arg0;
     }
     static displayScoreToLog(): void{
