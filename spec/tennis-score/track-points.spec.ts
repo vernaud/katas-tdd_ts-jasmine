19fbd7f5e1016bccd717529d93134159a630a19d
vernaud <40890244+vernaud@users.noreply.github.com>
2024-07-03 16:54:26 +0200

    refactor(test): add beforeEach()

--- spec/tennis-score/track-points.spec.ts
+++ spec/tennis-score/track-points.spec.ts
@@ -2,16 +2,21 @@ import { TrackPoints } from "../../src/tennis-score/track-points";
 
 describe('incrementPlayerPoints', () => {
 
+    let trackPoints: TrackPoints;
+
+    beforeEach(() => {
+        trackPoints = new TrackPoints();
+    });
+
     it('should increment the score of player', () => {
         const PLAYER1 = 1;
         const PLAYER2 = 2;
-        let tennisScore = new TrackPoints();
-        let lastPointsP1 = tennisScore.getWonPointsPlayer(PLAYER1);
-        let lastPointsP2 = tennisScore.getWonPointsPlayer(PLAYER2);
+        let lastPointsP1 = trackPoints.getWonPointsPlayer(PLAYER1);
+        let lastPointsP2 = trackPoints.getWonPointsPlayer(PLAYER2);
         
-        tennisScore.incrementPlayerPoints(PLAYER1);
-        let newPointsP1 = tennisScore.getWonPointsPlayer(PLAYER1);
-        let newPointsP2 = tennisScore.getWonPointsPlayer(PLAYER2);
+        trackPoints.incrementPlayerPoints(PLAYER1);
+        let newPointsP1 = trackPoints.getWonPointsPlayer(PLAYER1);
+        let newPointsP2 = trackPoints.getWonPointsPlayer(PLAYER2);
 
         expect(newPointsP1).toBeGreaterThan(lastPointsP1);
         expect(newPointsP2).toEqual(lastPointsP2);
@@ -19,8 +24,7 @@ describe('incrementPlayerPoints', () => {
 
     it('should throw an error when player is not in [1,2]', () => {
         expect(() => {
-            let tennisScore = new TrackPoints();
-            tennisScore.incrementPlayerPoints(3);
+            trackPoints.incrementPlayerPoints(3);
         }).toThrowError("Invalid player");
     });
 
